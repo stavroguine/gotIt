0049b1bbe37320421ab79dbdc6b2d0bd59569525
Flo <trash.anger@gmail.com>
2018-03-26 22:21:47 +0200

    Ajout de la suppression de questionnaire et de leur edition

--- routes/form.js
+++ routes/form.js
@@ -5,7 +5,7 @@ var Form = require('../tools/formtools');
 
 router.get('/', function(req, res, next) {
     res.redirect('/admin');
-  });
+});
 
 router.post('/', function(req, res, next) {
     var formData = {
@@ -16,15 +16,67 @@ router.post('/', function(req, res, next) {
         if(error){
             res.send(error);
         } else {
-            return res.redirect('/admin');
+            return res.redirect('/form');
+            return res.render('form', { name: req.body.name });
         }
     });
 });
 
-/* GET user page. */
+/* GET form details */
 router.get('/:name', function(req, res, next) {
-    return res.render('form', { name: req.params.name });
+    User.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        if (user === null) {
+          var err = new Error('Not authorized! Go back!');
+          err.status = 400;
+          return next(err);
+        } else {
+          if(req.session.role == "Admin"){
+            name = req.params.name;
+            Form.findOne({name: name}, function(err, form) {
+                console.log(form.name);
+                return res.render('form', { form: form });
+            });    
+          } else {
+            var err = new Error('Not authorized! Admin zone !!!');
+            err.status = 400;
+            return next(err);
+          }
+        }
+      }
+    });
 });
 
+/* GET form details */
+router.get('/:name/delete', function(req, res, next) {
+    User.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        if (user === null) {
+          var err = new Error('Not authorized! Go back!');
+          err.status = 400;
+          return next(err);
+        } else {
+          if(req.session.role == "Admin"){
+            name = req.params.name;
+            Form.remove({name: name}, function(err, form) {
+              Form.find({}, function(err, forms) {
+                return res.render('admin', { user: user, forms: forms });
+              });
+            });    
+          } else {
+            var err = new Error('Not authorized! Admin zone !!!');
+            err.status = 400;
+            return next(err);
+          }
+        }
+      }
+    });
+});
 
 module.exports = router; 
